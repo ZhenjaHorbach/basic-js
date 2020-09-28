ea8373b28a635673bb684be171cb72c23b6affb6
ZhenjaHorbach <HorbachZhenja@yandex.by>
2020-09-28 19:50:30 +0300

    feat: carbon-dating

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,15 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(disk,time) {
-	function Hanoi1(n, x, y, z){                       // переложить n дисков с x на y, используя {
-		if (n <= 0)                                     // (0) дисков нет - конец
+module.exports = function calculateHanoi(disk, time) {
+	let second = time / 3600;
+	function Hanoi(disk, x, y, z) {
+		if (disk <= 0)
 			return;
-
-		Hanoi1(n - 1, x, z, y);                            //     перекладываем n-1 дисков с x на z
-		document.write(x + '->' + y + ", ");                 // (1) переносим     1 диск     c x на y
-		Hanoi1(n - 1, z, y, x);                            //     перекладываем n-1 дисков с z на y
-
-		return;                                        // (2) конец (для наглядности листинга Hanoi3)
+		Hanoi(disk - 1, x, z, y);
+		Hanoi(disk - 1, z, y, x);
+		return;
 	}
 	throw new CustomError('Not implemented');
 	// remove line with error and write your code here
